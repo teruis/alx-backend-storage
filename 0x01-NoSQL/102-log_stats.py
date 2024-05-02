07cc31a5c41ce068eed3ad40399a73bd9f8a0cd0
teruis <126614240+teruis@users.noreply.github.com>
2024-05-02 08:35:40 +0100

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,42 +1,27 @@
 #!/usr/bin/env python3
-"""
-Log stats
-"""
+""" 102-log_stats """
 from pymongo import MongoClient
 
+def log_stats(mongo_collection):
+    total_logs = mongo_collection.count_documents({})
+    print(f"{total_logs} logs")
 
-def log_stats():
-    """ log_stats.
-    """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{total} logs")
-    print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-    print(f"{path} status check")
+    # Aggregation pipeline to count the occurrence of each IP
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}  # Limit to the top 10 IPs
+    ]
+
+    top_ips = list(mongo_collection.aggregate(pipeline))
+
     print("IPs:")
-    sorted_ips = logs_collection.aggregate(
-        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-         {"$sort": {"count": -1}}])
-    i = 0
-    for s in sorted_ips:
-        if i == 10:
-            break
-        print(f"\t{s.get('_id')}: {s.get('count')}")
-        i += 1
-
+    for ip_data in top_ips:
+        ip = ip_data["_id"]
+        count = ip_data["count"]
+        print(f"    {ip}: {count}")
 
 if __name__ == "__main__":
-    log_stats()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs_database.nginx  # Assuming nginx collection is in logs_database
+    log_stats(logs_collection)
